67d1f6aca8a11ad8c4c9b324af453ff42dd24ae0
Andrew Stephens <digitalmk@gmail.com>
2020-07-04 21:57:51 -0500

    Finished adding initial implementation of opcodes

--- main.go
+++ main.go
@@ -13,18 +13,32 @@ const (
 	FontsetStartAddress = 0x50
 )
 
+// Chip8 emulates a Chip-8 machine.
 type Chip8 struct {
-	registers [16]byte
-	memory    [4096]byte
-	index     uint16
-	pc        uint16
-	opcode    uint16
-	stack     [16]uint16
-	sp        byte
-	gfx       [2048]byte
-	dt        uint8
-	st        uint8
-	keypad    [16]byte
+	memory [4096]byte
+
+	// The Chip 8 has 16 registers. The first 15 registers are
+	// general purpose registers, with the 16th register being used
+	// for the "carry" flag.
+	V [16]byte
+
+	// There is one Index register (I)
+	I uint16
+
+	// The Program Counter (pc) is used to keep track
+	pc     uint16
+	opcode uint16
+	stack  [16]uint16
+	sp     byte
+
+	// The Chip 8 graphics system draws sprites to the screen.
+	// The screen is 64 x 32
+	gfx      [64 * 32]byte
+	drawFlag bool
+
+	delayTimer byte
+	soundTimer byte
+	keypad     [16]byte
 }
 
 func main() {
@@ -41,7 +55,7 @@ func main() {
 	// DEBUGGING
 	// fmt.Println(chip8.memory)
 	// fmt.Printf("Starting Address: %x \n", chip8.pc)
-	// fmt.Println("Array Length (bits): ", len(chip8.registers))
+	// fmt.Println("Array Length (bits): ", len(chip8.V))
 }
 
 func (ch *Chip8) loadGame(filename string) {
@@ -63,12 +77,20 @@ func (ch *Chip8) emulateCycle() {
 
 	ch.processOpcode(opCode)
 
-	ch.pc += 2
+	if ch.delayTimer > 0 {
+		ch.delayTimer--
+	}
+
+	if ch.soundTimer > 0 {
+		if ch.soundTimer == 1 {
+			fmt.Printf("BEEP!\n")
+			ch.soundTimer--
+		}
+	}
 }
 
 func (ch *Chip8) processOpcode(opCode uint16) {
-	fmt.Printf("Opcode: %#x \n", opCode)
-
+	fmt.Printf("Started Executing Opcode: %#x\n", opCode)
 	switch opCode & 0xF000 {
 	case 0x0000:
 		switch opCode & 0x000F {
@@ -84,45 +106,221 @@ func (ch *Chip8) processOpcode(opCode uint16) {
 		default:
 			fmt.Printf("Invalid opcode %X\n", opCode)
 		}
+
 	case 0x1000:
 		ch.pc = opCode & 0x0FFF
+
 	case 0x2000:
 		ch.stack[ch.sp] = ch.pc
 		ch.sp++
 		ch.pc = opCode & 0x0FFF
+
 	case 0x3000:
-		register := uint16(ch.registers[(opCode&0xF00)>>8])
+		register := uint16(ch.V[(opCode&0xF00)>>8])
 		value := opCode & 0x00FF
 		if register == value {
-			fmt.Println("skip instruction")
 			ch.pc += 4
 		} else {
 			ch.pc += 2
 		}
+
 	case 0x4000:
-		register := uint16(ch.registers[(opCode&0xF00)>>8])
+		register := uint16(ch.V[(opCode&0xF00)>>8])
 		value := opCode & 0x00FF
 		if register != value {
 			ch.pc += 4
 		} else {
 			ch.pc += 2
 		}
+
 	case 0x5000:
-		registerX := uint16(ch.registers[(opCode&0x0F00)>>8])
-		registerY := uint16(ch.registers[(opCode&0x00F0)>>8])
+		registerX := uint16(ch.V[(opCode&0x0F00)>>8])
+		registerY := uint16(ch.V[(opCode&0x00F0)>>8])
 		if registerX == registerY {
 			ch.pc += 4
 		} else {
 			ch.pc += 2
 		}
+
 	case 0x6000:
-		register := (opCode & 0x0F00) >> 8
-		value := byte(opCode & 0x00FF)
-		ch.registers[register] = value
+		x := (opCode & 0x0F00) >> 8
+		nn := byte(opCode & 0x00FF)
+		ch.V[x] = nn
 		ch.pc += 2
+
 	case 0x7000:
-		ch.registers[(opCode&0x0F00)>>8] = ch.registers[(opCode&0x0F00)>>8] + byte(opCode&0x00FF)
+		x := (opCode & 0x0F00) >> 8
+		nn := byte(opCode & 0x00FF)
+		ch.V[x] += nn
 		ch.pc += 2
+
+	case 0x8000:
+		x := (opCode & 0x0F00) >> 8
+		y := (opCode & 0x00F0) >> 4
+		switch opCode & 0x000F {
+		case 0x0000:
+			ch.V[x] = ch.V[y]
+			ch.pc += 2
+		case 0x0001:
+			ch.V[x] |= ch.V[y]
+			ch.pc += 2
+		case 0x0002:
+			ch.V[x] &= ch.V[y]
+			ch.pc += 2
+		case 0x0003:
+			ch.V[x] ^= ch.V[y]
+			ch.pc += 2
+		case 0x0004:
+			if (ch.V[x] + ch.V[y]) > 0xFF {
+				ch.V[0xF] = 1
+			} else {
+				ch.V[0xF] = 0
+			}
+
+			ch.V[x] += ch.V[y]
+			ch.pc += 2
+		case 0x0005:
+			if ch.V[y] > ch.V[x] {
+				ch.V[0xF] = 0
+			} else {
+				ch.V[0xF] = 1
+			}
+
+			ch.V[x] -= ch.V[y]
+			ch.pc += 2
+		case 0x0006:
+			ch.V[x] = ch.V[y] >> 1
+			ch.V[0xF] = ch.V[y] & 0x01
+			ch.pc += 2
+		case 0x0007:
+			if ch.V[x] > ch.V[y] {
+				ch.V[0xF] = 0
+			} else {
+				ch.V[0xF] = 1
+			}
+
+			ch.V[x] = ch.V[y] - ch.V[x]
+			ch.pc += 2
+		case 0x000E:
+			ch.V[0xF] = (ch.V[x] & 0x80) >> 7
+			ch.V[x] = ch.V[x] << 1
+
+		}
+
+	case 0x9000:
+		x := (opCode & 0x0F00) >> 8
+		y := (opCode & 0x00F0) >> 4
+
+		if ch.V[x] != ch.V[y] {
+			ch.pc += 4
+		} else {
+			ch.pc += 2
+		}
+
+	case 0xA000:
+		ch.I = opCode & 0x0FFF
+		ch.pc += 2
+
+	case 0xB000:
+		nnn := opCode & 0x0FFF
+		ch.pc = uint16(ch.V[0]) + nnn
+
+	case 0xC000:
+		x := uint16(opCode&0x0F00) >> 8
+		nn := opCode & 0x00FF
+		ch.V[x] = byte(randomByte()) & byte(nn)
+		ch.pc += 2
+
+	case 0xD000:
+		x := uint16(ch.V[(opCode&0x0F00)>>8])
+		y := uint16(ch.V[(opCode&0x00F0)>>4])
+		height := opCode & 0x000F
+		var pixel uint16
+
+		ch.V[0xF] = 0
+		for yline := uint16(0); yline < height; yline++ {
+			pixel = uint16(ch.memory[ch.I+yline])
+			for xline := uint16(0); xline < 8; xline++ {
+				index := (x + xline + ((y + yline) * 64))
+				if index > uint16(len(ch.gfx)) {
+					continue
+				}
+				if (pixel & (0x80 >> xline)) != 0 {
+					if ch.gfx[index] == 1 {
+						ch.V[0xF] = 1
+					}
+					ch.gfx[index] ^= 1
+				}
+			}
+		}
+
+		ch.drawFlag = true
+		ch.pc += 2
+
+	case 0xE000:
+		x := (opCode & 0xF00) >> 8
+		switch opCode & 0x00FF {
+		case 0x009E:
+			if ch.keypad[ch.V[x]] != 0 {
+				ch.pc += 4
+				ch.keypad[ch.V[x]] = 0
+			} else {
+				ch.pc += 2
+			}
+		case 0x00A1:
+			if ch.keypad[ch.V[x]] == 0 {
+				ch.pc += 4
+			} else {
+				ch.keypad[ch.V[x]] = 0
+				ch.pc += 2
+			}
+		}
+
+	case 0xF000:
+		x := (opCode & 0x0F00) >> 8
+		switch opCode & 0x00FF {
+		case 0x0007:
+			ch.V[x] = ch.delayTimer
+			ch.pc += 2
+		case 0x000A:
+			for index, k := range ch.keypad {
+				if k != 0 {
+					ch.V[x] = byte(index)
+					ch.pc += 2
+					break
+				}
+			}
+			ch.keypad[ch.V[x]] = 0
+		case 0x0015:
+			ch.delayTimer = ch.V[x]
+			ch.pc += 2
+		case 0x0018:
+			ch.soundTimer = ch.V[x]
+			ch.pc += 2
+		case 0x001E:
+			ch.I += uint16(ch.V[x])
+			ch.pc += 2
+		case 0x0029:
+			ch.I = uint16(ch.V[x]) * 5
+			ch.pc += 2
+		case 0x0033:
+			ch.memory[ch.I] = ch.V[x] / 100
+			ch.memory[ch.I+1] = (ch.V[x] / 10) % 10
+			ch.memory[ch.I+2] = (ch.V[x] % 100) % 10
+			ch.pc += 2
+		case 0x0055:
+			for i := uint16(0); i <= x; i++ {
+				ch.memory[ch.I+i] = ch.V[i]
+			}
+			ch.pc += 2
+		case 0x0065:
+			for i := uint16(0); i <= x; i++ {
+				ch.V[i] = ch.memory[ch.I+i]
+			}
+			ch.pc += 2
+		default:
+			fmt.Printf("Unknown Opcode 0x%X", opCode)
+		}
 	}
 }
 
